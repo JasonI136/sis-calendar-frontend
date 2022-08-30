8e4c905c9d23c8397b22f6557697959e49aa142d
Jason Immanuel <jason.immanuel136@gmail.com>
2022-08-30 22:01:36 +1000

    timed views, basic data binding

--- src/pages/calendar-view/calendar-view.js
+++ src/pages/calendar-view/calendar-view.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './calendar-view.scss';
 import { Scheduler, Editing } from 'devextreme-react/scheduler';
+import { data } from './data.js';
 
+const currentDate = new Date(2021, 2, 28);
 export default function calendarView() {
 
-  const views = ['week', 'timelineMonth', 'agenda'];
+  const views = ['day', 'week', 'month'];
 
   return (
-    <Scheduler id="scheduler" views={views} currentView="week">
-      {
-        <Editing allowDragging={false}/>
-      }
+    <React.Fragment>
+      <h2 className={'content-block'}>Tasks</h2>
       
-    </Scheduler>
+      <Scheduler 
+        id="scheduler" 
+        views={views} 
+        defaultCurrentView="week"
+        defaultCurrentDate={currentDate}
+        startDayHour={9}
+        dataSource={data}
+      >
+        
+        {
+          <Editing allowDragging={true}/>
+        }
+        
+      </Scheduler>
+    </React.Fragment>
   );
 
 }
